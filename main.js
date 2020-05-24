01d12d7386f071c43b4e04e129fb2a891c4ea069
Mateusz Kuczaj <kuczajmateusz@gmail.com>
2020-05-24 19:50:34 +0200

    add js innerhtml

---
+++ main.js
@@ -0,0 +1,9 @@
+const firstName = 'Mateo';
+const age = 32;
+
+console.log(`siema jestem  ${firstName}  i mam ${age} lata`);
+console.log(age);
+const dupa = document.querySelector('.omnnie_descryption');
+dupa.innerHTML = `siema jestem  ${firstName}  i mam ${age} lata`;
+const pusta = document.querySelector('.pusta');
+pusta.innerHTML = `to jeste tajemna notatka`;
